a47bc9f57d9106e390c4e56964556c4b199cb71c
Mhayk Whandson <eu@mhayk.com.br>
2017-04-24 19:45:28 -0400

    1016 solved!
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ 1016/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int distance;
+
+    cin >> distance;
+    cout << distance*2 << " minutos" << endl;
+    return 0;
+}
