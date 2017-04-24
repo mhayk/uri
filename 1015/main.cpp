1ec19340cf8685113bf77af688fbe3ef213f7cc0
Mhayk Whandson <eu@mhayk.com.br>
2017-04-24 19:39:37 -0400

    1015 solved!
    distance between two points;
    
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ 1015/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    double x1,y1,x2,y2;
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
+
+    cout << fixed << setprecision(4) << sqrt(pow((x2-x1),2)+pow((y2-y1),2)) << endl;
+    return 0;
+}
