ef17e2ae2612af4dd777a0245f10a3bde4a7841d
Mhayk Whandson <eu@mhayk.com.br>
2017-05-02 20:18:53 -0400

    1052 solved!
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ 1052/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int month;
+
+    cin >> month;
+
+    switch(month)
+    {
+    case 1:
+        cout << "Janeiro" << endl;
+        break;
+    case 2:
+        cout << "Fevereiro" << endl;
+        break;
+    case 3:
+        cout << "Março" << endl;
+        break;
+    case 4:
+        cout << "Abril" << endl;
+        break;
+    case 5:
+        cout << "Maio" << endl;
+        break;
+    case 6:
+        cout << "Junho" << endl;
+        break;
+    case 7:
+        cout << "Julho" << endl;
+        break;
+    case 8:
+        cout << "Agosto" << endl;
+        break;
+    case 9:
+        cout << "Setembro" << endl;
+        break;
+    case 10:
+        cout << "Outubro" << endl;
+        break;
+    case 11:
+        cout << "Novembro" << endl;
+        break;
+    case 12:
+        cout << "Dezembro" << endl;
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
