4124b144a48f9d80db35493e875ff2e54fc64729
Mhayk Whandson <eu@mhayk.com.br>
2017-05-06 18:04:27 -0400

    1078 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1078/1078/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=1; i <= 10; i++)
+            {
+                Console.WriteLine(i + " x " + num + " = " + num * i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
