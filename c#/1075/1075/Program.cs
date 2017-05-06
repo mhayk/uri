3efcdfff95e8ff99233c0da0e9536d8160f6376c
Mhayk Whandson <eu@mhayk.com.br>
2017-05-06 17:58:32 -0400

    1075 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1075/1075/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1075
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=1; i<=10000; i++)
+            {
+                if ((i % num) == 2)
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
