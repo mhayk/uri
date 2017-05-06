e0f16e3a036e665493ce047eeb274b550c54f9ae
Mhayk Whandson <eu@mhayk.com.br>
2017-05-06 17:18:08 -0400

    1073 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1073/1073/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1073
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if( (i % 2) == 0)
+                {
+                    Console.WriteLine(i + "^2 = " + Math.Pow(i, 2));
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
