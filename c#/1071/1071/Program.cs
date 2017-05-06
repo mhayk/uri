65f91415c929b90185fd7f61af185609d38a5372
Mhayk Whandson <eu@mhayk.com.br>
2017-05-06 10:51:44 -0400

    1071 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1071/1071/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1071
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int x, y, soma = 0;
+
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if ((x % 2) == 0)
+            {
+                for (int i = x - 1; i > y; i -= 2)
+                {
+                    soma += i;
+                }
+            }
+            else
+            {
+                for (int i = x - 2; i > y; i -= 2)
+                {
+                    soma += i;
+                }
+            }
+            Console.WriteLine(soma);
+
+            Console.ReadKey();
+        }
+    }
+
+}
