da966530e206913aea512fd713619dc4438e37da
Mhayk Whandson <eu@mhayk.com.br>
2017-05-06 19:43:58 -0400

    1080 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1080/1080/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,position,aux;
+
+            num = aux = position = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                aux = Int32.Parse(Console.ReadLine());
+                if (num < aux)
+                {
+                    num = aux;
+                    position = i;
+                }
+            }
+            Console.WriteLine(num);
+            Console.WriteLine(position);
+
+            Console.ReadLine();
+        }
+    }
+}
