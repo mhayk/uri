8c8f9c695e20431ddd1b4c400203b26e391d3892
Mhayk Whandson <eu@mhayk.com.br>
2017-05-08 17:54:53 -0400

    1097 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1097/1097/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1097
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j,l;
+            l = 7;
+            for (i=1; i <= 9; i+=2)
+            {
+                
+                for(j=l; j >= l-2; j-- )
+                {
+                    Console.WriteLine("I={0} J={1}",i,j);
+                }
+                l += 2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
