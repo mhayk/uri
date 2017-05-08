16506cac5a61776b4e5fef38ceae9f38c6b0618a
Mhayk Whandson <eu@mhayk.com.br>
2017-05-08 00:04:31 -0400

    1095 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1095/1095/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1095
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, j = 60;
+            while(j>=0)
+            {
+                Console.WriteLine("I={0} J={1}",i,j);
+                i += 3;
+                j -= 5;
+            }
+            Console.ReadLine();
+        }
+    }
+}
