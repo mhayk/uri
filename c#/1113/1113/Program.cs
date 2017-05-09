aa74f76ae870d1fb90167638f753db0690c3c897
Mhayk Whandson <eu@mhayk.com.br>
2017-05-09 17:11:56 -0400

    1113 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1113/1113/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num;
+            int a, b, min, max, sum;
+
+
+            while ((num = Console.ReadLine().Split(' ')) != null)
+            {
+                a = Convert.ToInt32(num[0]);
+                b = Convert.ToInt32(num[1]);
+
+                if (a == b )
+                    return;
+
+                if (a < b)
+                    Console.WriteLine("Crescente");
+                else
+                    Console.WriteLine("Decrescente");
+
+            }
+        }
+    }
+}
