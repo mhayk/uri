8ad14241a997dad7146d5a3ff3b220cd00594dbf
Mhayk Whandson <eu@mhayk.com.br>
2017-05-09 17:16:21 -0400

    1114 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1114/1114/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num;
+
+            while ((num = Console.ReadLine()) != null)
+            {
+                if (num == "2002")
+                {
+                    Console.WriteLine("Acesso Permitido");
+                    return;
+                }
+                else
+                    Console.WriteLine("Senha Invalida");
+            }
+        }
+    }
+}
