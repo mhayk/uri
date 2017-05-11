b5f0336ca0a1b2ff594738e830e9c3137114540f
Mhayk Whandson <eu@mhayk.com.br>
2017-05-10 21:05:52 -0400

    1115 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1115/1115/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num;
+            int x, y;
+
+            while( (num = Console.ReadLine().Split(' ')) != null)
+            {
+                x = Convert.ToInt32(num[0]);
+                y = Convert.ToInt32(num[1]);
+
+                if (x == 0 || y == 0)
+                    return;
+
+                if ((x > 0) && (y > 0))
+                    Console.WriteLine("primeiro");
+
+                if ((x < 0) && (y > 0))
+                    Console.WriteLine("segundo");
+
+                if ((x < 0) && (y < 0))
+                    Console.WriteLine("terceiro");
+
+                if ((x > 0) && (y < 0))
+                    Console.WriteLine("quarto");
+            }
+        }
+    }
+}
