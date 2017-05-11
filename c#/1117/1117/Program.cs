f9ee2f75bc9e5249f6c3384bfaabb73dc547aa38
Mhayk Whandson <eu@mhayk.com.br>
2017-05-10 22:09:57 -0400

    1117 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1117/1117/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num;
+            List<decimal> list = new List<decimal>();
+            
+            do
+            {
+                num = Convert.ToDecimal(Console.ReadLine());
+                if ((num > 0) && (num <= 10))
+                    list.Add(num);
+                else
+                    Console.WriteLine("nota invalida");
+
+            }
+            while ( list.Count() < 2);
+            Console.WriteLine("media = {0:0.00}", (list.ElementAt(0)+list.ElementAt(1))/2);
+            Console.ReadLine();
+        }
+    }
+}
