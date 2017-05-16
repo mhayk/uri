45c8d47ea8dc1e872cefe9a28be5b55f09b46adf
Mhayk Whandson <eu@mhayk.com.br>
2017-05-16 19:37:36 -0400

    1134 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1134/1134/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalAlcool, totalGasolina, totalDiesel, value;
+
+            totalAlcool = totalGasolina = totalDiesel = 0;
+
+            do
+            {
+                value = Convert.ToInt32(Console.ReadLine());
+                switch (value)
+                {
+                    case 1:
+                        totalAlcool++;
+                        break;
+                    case 2:
+                        totalGasolina++;
+                        break;
+                    case 3:
+                        totalDiesel++;
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (value != 4);
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: {0}",totalAlcool);
+            Console.WriteLine("Gasolina: {0}",totalGasolina);
+            Console.WriteLine("Diesel: {0}", totalDiesel);
+            Console.ReadLine();
+        }
+    }
+}
