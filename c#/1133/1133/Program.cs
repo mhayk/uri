eaad33e74f7e85671e0aef81867247d00040f5d4
Mhayk Whandson <eu@mhayk.com.br>
2017-05-16 19:24:06 -0400

    1133 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1133/1133/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1133
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            if (x < y)
+            {
+                for (int i = x+1; i < y; i++)
+                {
+                    if (((i % 5) == 2) || ((i % 5) == 3))
+                        Console.WriteLine(i);
+                }
+            }
+            else if(x > y)
+            {
+                for (int i = y+1; i <= x; i++)
+                {
+                    if ( ((i % 5) == 2) || ((i % 5) == 3) )
+                        Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
