f9fcabe5710b68a3e970e0cf77ece4e20898a4fc
Mhayk Whandson <eu@mhayk.com.br>
2017-05-18 18:39:38 -0400

    1145 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1145/1145/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string[] input;
+
+            input = Console.ReadLine().Split(' ');
+            x = Convert.ToInt32(input[0]);
+            y = Convert.ToInt32(input[1]);
+
+            for (int i=1; i<=y; i++)
+            {
+                
+                if ((i % x) == 0)
+                    Console.WriteLine(i);
+                else
+                    Console.Write("{0} ", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
