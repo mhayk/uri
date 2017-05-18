bc0a30760653bd1d77360b5d7a0955e73cfc1be0
Mhayk Whandson <eu@mhayk.com.br>
2017-05-18 18:21:23 -0400

    1144 solved! C#
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c#/1144/1144/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1144
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=1; i<=n; i++)
+            {
+                Console.WriteLine("{0} {1} {2}",i,Math.Pow(i,2),Math.Pow(i,3));
+                Console.WriteLine("{0} {1} {2}",i,Math.Pow(i, 2)+1, Math.Pow(i, 3)+1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
