c9d638e5326312bdd19231b32e5cee7fe43b5fbc
Mhayk Whandson <eu@mhayk.com.br>
2017-10-22 19:44:55 -0400

    1172 solved! C++
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

---
+++ c++/1172/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int X[10],i;
+    for(i=0; i < 10; i++)
+    {
+        cin >> X[i];
+        if(X[i] == 0 || X[i] < 0)
+            X[i] = 1;
+    }
+    for(i=0; i < 10; i++)
+    {
+        cout << "X[" << i << "] = " << X[i] << endl;
+    }
+    return 0;
+}
+
